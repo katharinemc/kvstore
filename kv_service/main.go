df4e5ddc7a3df28ac71bc664e06038ab9e145971
Katharine McLeod <katharine.mcleod@expel.io>
2025-07-30 10:28:52 -0400

    service testing, fixes; needs: refactor for simplicity;  client; documentation

--- kv_service/main.go
+++ kv_service/main.go
@@ -28,7 +28,7 @@ func startKVStore(reqCh <-chan KVRequest) {
 		case "set":
 			log.Println("service, line 28")
 			store[req.Key] = req.Value
-			req.Resp <- KVResponse{Success: true}
+			req.Resp <- KVResponse{Success: true, Value: req.Value}
 		case "get":
 			val, ok := store[req.Key]
 			req.Resp <- KVResponse{Success: ok, Value: val}
@@ -44,9 +44,9 @@ var updatesToStoreChannel = make(chan KVRequest)
 
 func setKey(c *gin.Context) {
 	key := c.Param("key")
-	log.Println("line 46: set key operation in progress for key", key)
 
 	var body struct {
+		Key   string `json:"key"`
 		Value string `json:"value"`
 	}
 	if err := json.NewDecoder(c.Request.Body).Decode(&body); err != nil {
@@ -58,8 +58,7 @@ func setKey(c *gin.Context) {
 	updatesToStoreChannel <- KVRequest{Method: "set", Key: key, Value: body.Value, Resp: setResponseChannel} // pushes requests to channel being consumed by startKVStore function
 	resp := <-setResponseChannel
 
-	c.JSON(http.StatusOK, gin.H{"key": key, "value": resp.Value, "message": "nice job"}) // Send JSON response
-
+	c.JSON(http.StatusCreated, gin.H{"key": key, "value": resp.Value})
 }
 
 func getKey(c *gin.Context) {
@@ -89,17 +88,22 @@ func deleteKey(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"whatwhat!": resp.Success})
 }
 
-func main() {
-
-	log.Println("service, line 94")
-
-	go startKVStore(updatesToStoreChannel) // using a goroutine to avoid weird race behavior writing/deleting the same key at the same time.
-
+func setupRouter() *gin.Engine {
 	r := gin.Default()
-	// skipping POST since they key is always known.
 	r.PATCH("/kv/:key", setKey) // In this simple environment, setting a key is always a PUT because there's only one value that is always overwrititen; there's nothing to partially update. BUT, PUT makes me nervous and in a real production environment with larger objects, PATCH would be safer.
 	r.GET("/kv/:key", getKey)
 	r.DELETE("/kv/:key", deleteKey)
+	r.POST("/kv/", func(c *gin.Context) { // skipping POST since they key is always known.
+		c.JSON(http.StatusMethodNotAllowed, gin.H{"error": "Method Not Allowed"})
+	})
+	return r
+}
+
+func main() {
+
+	go startKVStore(updatesToStoreChannel) // using a goroutine to avoid race behavior writing/deleting the same key at the same time.
+
+	r := setupRouter()
 
 	log.Println("KV Service listening on :8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
---
+++ kv_service/service_test.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMain(t *testing.T) {
+	testCases := []struct {
+		description        string
+		input              []byte
+		expectedStatusCode int
+		method             string
+		key                string
+		expectedResponse   string
+	}{
+
+		{
+			description:        "success: GET key/value",
+			input:              []byte(`{}`),
+			expectedStatusCode: http.StatusOK,
+			method:             http.MethodGet,
+			key:                "robin",
+			expectedResponse:   "{\"key\":\"robin\",\"value\":\"jasonTodd\"}",
+		},
+		{
+			description:        "success: PATCH creates new key/value when key does not previously exist", //for maximum effect in a production environment, I would refactor so this test could include an initial GET to demonstrate the key does not exist before the operation. For today, observe the data seeded in the test includes no toast.
+			input:              []byte(`{"key": "batman","value": "bruceWayne"}`),
+			method:             http.MethodPatch,
+			key:                "batman",
+			expectedStatusCode: http.StatusCreated,
+			expectedResponse:   "{\"key\":\"batman\",\"value\":\"bruceWayne\"}",
+		},
+		{
+			description:        "success: PATCH key/value when exists",
+			input:              []byte(`{"key": "robin","value": "dickGrayson"}`),
+			method:             http.MethodPatch,
+			key:                "robin",
+			expectedStatusCode: http.StatusCreated,
+			expectedResponse:   "{\"key\":\"robin\",\"value\":\"dickGrayson\"}",
+		},
+
+		// // {description: "success: DELETE key/value"},
+		{
+			description:        "error: GET, key does not exist",
+			input:              []byte(`{}`),
+			expectedStatusCode: http.StatusNotFound,
+			method:             http.MethodGet,
+			key:                "bigfoot",
+			expectedResponse:   "key not found\n",
+		},
+		// {description: "error: DELETE, key does not exist"},
+		{
+			description:        "error: PATCH request invalid",
+			input:              []byte(`{"garbageData"}`),
+			method:             http.MethodPatch,
+			key:                "batman",
+			expectedStatusCode: http.StatusBadRequest,
+			expectedResponse:   "invalid body\n",
+		},
+		{
+			description:        "error: POST not supported",
+			input:              []byte(`{"key": "superman","value": "clarkKent"}`),
+			method:             http.MethodPost,
+			expectedStatusCode: http.StatusMethodNotAllowed,
+			expectedResponse:   "{\"error\":\"Method Not Allowed\"}",
+		},
+	}
+	for _, tc := range testCases {
+
+		go startKVStore(updatesToStoreChannel)
+
+		seedResp := make(chan KVResponse)
+		updatesToStoreChannel <- KVRequest{
+			Method: "set",
+			Key:    "robin",
+			Value:  "jasonTodd",
+			Resp:   seedResp,
+		}
+		<-seedResp
+
+		testRouter := setupRouter()
+
+		req, err := http.NewRequest(tc.method, "/kv/"+tc.key, bytes.NewBuffer(tc.input))
+		assert.NoError(t, err)
+
+		t.Run(tc.description, func(t *testing.T) {
+			rw := httptest.NewRecorder()
+			testRouter.ServeHTTP(rw, req)
+
+			assert.Equal(t, tc.expectedStatusCode, rw.Code)
+			assert.Equal(t, tc.expectedResponse, rw.Body.String())
+		})
+	}
+
+}
